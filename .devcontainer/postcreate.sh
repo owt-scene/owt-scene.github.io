625d9b2de7b356b4310150017ad96afc7d684b7b
Joshua Rich <joshua.rich@gmail.com>
2024-10-14 09:48:39 +1000

    build(devcontainer): :sparkles: install starship in shell

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2023 Joshua Rich <joshua.rich@gmail.com>
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
 set -e
 
+# Add hugo shell completions.
 sudo /usr/local/hugo/bin/hugo completion bash | sudo tee /etc/bash_completion.d/hugo > /dev/null
 sudo /usr/local/hugo/bin/hugo completion fish | sudo tee /etc/fish/completions/hugo > /dev/null
+
+# Install and configure starship.
+curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
+mkdir -p ~/.config/fish
+echo "starship init fish | source" >>~/.config/fish/config.fish
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+
+exit 0
